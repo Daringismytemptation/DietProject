7c16ab53ef6e34621d51b246802dbdfe7a081def
Mertcan <akcamurmertcan@gmail.com>
2022-12-18 21:41:34 +0300

    Ege Mertcan Batu yazdım

--- DietProject/Program.cs
+++ DietProject/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,6 +18,7 @@ namespace DietProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            int a = 7;
         }
     }
 }
