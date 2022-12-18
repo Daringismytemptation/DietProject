4c8a317cb7fe02978ea0299aa8ee2460a62ff3db
egesumer <egesumer@windowslive.com>
2022-12-18 21:58:13 +0300

    batuhan yazdım

--- DietProject/Program.cs
+++ DietProject/Program.cs
@@ -19,7 +19,7 @@ namespace DietProject
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
            
-
+            string name = "batuhan";
         }
     }
 }
