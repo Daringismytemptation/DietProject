7372e9c3f6809944ce9b8a8c107ac78a28171207
egesumer <egesumer@windowslive.com>
2022-12-26 00:06:02 +0300

    pwui

--- DietProject/frmChangePassword.Designer.cs
+++ DietProject/frmChangePassword.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmChangePassword));
             this.btnSifreDegistir = new System.Windows.Forms.Button();
             this.txtYeniSifreTekrar = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -39,62 +40,78 @@
             // 
             // btnSifreDegistir
             // 
-            this.btnSifreDegistir.Location = new System.Drawing.Point(17, 220);
+            this.btnSifreDegistir.BackColor = System.Drawing.Color.Transparent;
+            this.btnSifreDegistir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSifreDegistir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSifreDegistir.ForeColor = System.Drawing.Color.Gold;
+            this.btnSifreDegistir.Location = new System.Drawing.Point(17, 229);
             this.btnSifreDegistir.Name = "btnSifreDegistir";
-            this.btnSifreDegistir.Size = new System.Drawing.Size(378, 37);
+            this.btnSifreDegistir.Size = new System.Drawing.Size(260, 37);
             this.btnSifreDegistir.TabIndex = 55;
             this.btnSifreDegistir.Text = "Şifre Değiştir";
-            this.btnSifreDegistir.UseVisualStyleBackColor = true;
+            this.btnSifreDegistir.UseVisualStyleBackColor = false;
             this.btnSifreDegistir.Click += new System.EventHandler(this.btnSifreDegistir_Click);
             // 
             // txtYeniSifreTekrar
             // 
             this.txtYeniSifreTekrar.Location = new System.Drawing.Point(166, 154);
             this.txtYeniSifreTekrar.Name = "txtYeniSifreTekrar";
-            this.txtYeniSifreTekrar.Size = new System.Drawing.Size(229, 20);
+            this.txtYeniSifreTekrar.Size = new System.Drawing.Size(111, 20);
             this.txtYeniSifreTekrar.TabIndex = 54;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.DarkKhaki;
             this.label3.Location = new System.Drawing.Point(13, 157);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.Size = new System.Drawing.Size(107, 13);
             this.label3.TabIndex = 53;
             this.label3.Text = "Yeni Sifre Tekrar:";
             // 
             // txtyeniSifre
             // 
-            this.txtyeniSifre.Location = new System.Drawing.Point(166, 84);
+            this.txtyeniSifre.Location = new System.Drawing.Point(167, 106);
             this.txtyeniSifre.Name = "txtyeniSifre";
-            this.txtyeniSifre.Size = new System.Drawing.Size(229, 20);
+            this.txtyeniSifre.Size = new System.Drawing.Size(111, 20);
             this.txtyeniSifre.TabIndex = 52;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 87);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.DarkKhaki;
+            this.label2.Location = new System.Drawing.Point(14, 109);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(74, 13);
             this.label2.TabIndex = 51;
             this.label2.Text = "Yeni Sifre : ";
             // 
             // txtEskiSifre
             // 
-            this.txtEskiSifre.Location = new System.Drawing.Point(166, 16);
+            this.txtEskiSifre.Location = new System.Drawing.Point(167, 54);
             this.txtEskiSifre.Name = "txtEskiSifre";
-            this.txtEskiSifre.Size = new System.Drawing.Size(229, 20);
+            this.txtEskiSifre.Size = new System.Drawing.Size(111, 20);
             this.txtEskiSifre.TabIndex = 50;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkKhaki;
+            this.label1.Location = new System.Drawing.Point(14, 57);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(69, 13);
             this.label1.TabIndex = 49;
             this.label1.Text = "Eski Sifre :";
             // 
@@ -102,7 +119,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(419, 269);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(305, 321);
             this.Controls.Add(this.btnSifreDegistir);
             this.Controls.Add(this.txtYeniSifreTekrar);
             this.Controls.Add(this.label3);
@@ -110,7 +129,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtEskiSifre);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmChangePassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmChangePassword";
             this.ResumeLayout(false);
             this.PerformLayout();
